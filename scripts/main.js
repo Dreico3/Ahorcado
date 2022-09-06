0264d0d59c316e80f088bef6900c4cf00acadab6
Erick <fis9.81@gmail.com>
2022-09-06 08:25:17 -0400

    interfas cell acabada solo faltaria mosrtrar las letras erroneas

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,20 @@ const btnDesistir=document.querySelector('#btn-rendirse');
 const btnGuardarEmpezar=document.querySelector('#btn-guardar');
 const cajaAdivina=document.querySelector(".adivina");
 const btnNuevoJuego=document.querySelector("#botones-jugar");
+const btnReiniciar=document.querySelector('#reiniciarPer');
+const btnReiniciarGa=document.querySelector('#reiniciarGa');
 
 var palabras=["perro", "card", "los", "un"];
+var fallas=0;
+var aciertos=0;
 function VaciarCaja(){
     while (cajaAdivina.firstChild){
         cajaAdivina.removeChild(cajaAdivina.firstChild);
     };
+    while(fallas>0){
+        document.querySelector(`#c${fallas}`).style.display="none";
+        fallas-=1;
+    }
 }
 function ReiniciarJuego(){
     VaciarCaja();
@@ -28,12 +36,24 @@ function Cancelar(){
 }
 
 function ControladorTeclas(e){
-    let palabra=palabras[idPalabra];
-    if(e.target.value==palabra[e.target.id-1]){
-        console.log("son iguales");
-    }else{
-        console.log("fallaste perra");
-    }
+        let palabra=palabras[idPalabra];
+        if(e.target.value.toLowerCase()==palabra[e.target.id-1]){
+            console.log("son iguales");
+            aciertos+=1;
+            
+        }else{
+            console.log("fallaste perra");
+            fallas+=1
+            console.log(fallas);
+            document.querySelector(`#c${fallas}`).style.display="block";
+        }
+        if(aciertos>=palabra.length){
+            document.querySelector('.cont-ganador').style.display="block";
+        }
+        if(fallas>=6){
+            document.querySelector('.cont-perdedor').style.display="block";
+            document.querySelector('#palabra__ganadora').innerHTML=palabra;
+        }
 }
 function EsconderPalabra(pa){
     //console.log(retornaAleatorio());
@@ -43,6 +63,7 @@ function EsconderPalabra(pa){
         ele.setAttribute("id", i);
         ele.setAttribute("class", "pa");
         ele.setAttribute("maxlength", "1");
+        ele.setAttribute("autocomplete","off");
         ele.onchange=ControladorTeclas;
         cajaAdivina.appendChild(ele);
     }
@@ -63,13 +84,23 @@ function Rendirse(){
     document.querySelector("#botones-jugar").style.display="none";
     document.querySelector(".cont-jugar").style.display="none";
 }
+function Reiniciar(){
+    document.querySelector('.cont-perdedor').style.display="none";
+    document.querySelector('.cont-ganador').style.display="none";
+    Rendirse()
+    aciertos=0;
+}
 function GuardarEmpezar(){
-    Cancelar()
-    IniciarJuego();
-    var palabra=document.querySelector("#inp-palabra").value;
-    palabras.push(palabra);
-    console.log(palabras);
-    document.querySelector("#inp-palabra").value="";
+    var palabra=document.querySelector("#inp-palabra").value.toLowerCase();
+    if(palabra.length<8){
+        Cancelar()
+        IniciarJuego();
+        palabras.push(palabra);
+        console.log(palabras);
+        document.querySelector("#inp-palabra").value="";
+    }else{
+        alert("la palabra introducida tiene mas de 8 letras")
+    }
 }
 btnNuevaPalabra.onclick = CambiarAagregar; 
 btnCancelar.onclick=Cancelar;
@@ -77,7 +108,8 @@ btnIniciarJuego.onclick=IniciarJuego;
 btnDesistir.onclick=Rendirse;
 btnGuardarEmpezar.onclick=GuardarEmpezar;
 btnNuevoJuego.onclick=ReiniciarJuego;
-
+btnReiniciar.onclick=Reiniciar;
+btnReiniciarGa.onclick=Reiniciar;
 
 
 /* const el = document.querySelector('.selector');
